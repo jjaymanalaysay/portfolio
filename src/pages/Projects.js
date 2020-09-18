2b0f3aca54613a5cdf1473f92b12c234d548c8ec
John joseph Manalaysay <jjaymanalaysay@gmail.com>
2020-09-19 05:58:54 +0800

    All contents inserted but need more fixing on the UIs

---
+++ src/pages/Projects.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import customer from '../images/customer-dash.jpg'
+import dash from '../images/dash.jpg'
+import ecom from '../images/ecom.jpg'
+
+export default function Projects() {
+    return (
+      <>
+        <section id="projects" className="section1">
+          <h3>Projects</h3>
+          <p>"The best way to predict the future, is to create it"</p>
+          <p> ~ Peter Drucker</p>
+          <div className="post-wrapper">
+            <div>
+              <div className="post">
+                <img className="thumbnail" src={dash} alt="dashpic" />
+                <div className="post-preview">
+                  <h6 className="post-title">Project 1 / ReactJS</h6>
+                  <p className="post-intro">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Minima, tenetur!
+                  </p>
+                  <button>
+                    <a href="https://github.com/jjaymanalaysay">
+                      <i className="fab fa-github"></i>Github
+                    </a>
+                  </button>
+                  <button>
+                    <a href="#">
+                      <i className="fa fa-globe"></i>Website
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="post">
+                <img className="thumbnail" src={ecom} />
+                <div className="post-preview">
+                  <h6 className="post-title">Project 2 /</h6>
+                  <p className="post-intro">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Ipsa, harum.
+                  </p>
+                  <button>
+                    <a href="https://github.com/jjaymanalaysay">
+                      <i className="fab fa-github"></i>Github
+                    </a>
+                  </button>
+                  <button>
+                    <a href="#">
+                      <i className="fa fa-globe"></i>Website
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div className="post">
+                <img className="thumbnail" src={customer} />
+                <div className="post-preview">
+                  <h6 className="post-title">Project 3 / </h6>
+                  <p className="post-intro">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Dolor, odit?
+                  </p>
+                  <button>
+                    <a href="#">
+                      <i className="fab fa-github"></i>Github
+                    </a>
+                  </button>
+                  <button>
+                    <a href="https://github.com/jjaymanalaysay">
+                      <i className="fa fa-globe"></i>Website
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+}
